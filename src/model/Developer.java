29989e7eff7c68461e2e1281f116f74a38c4dba9
cemreguvenilir <guvenilircemre@gmail.com>
2023-07-31 11:21:55 +0300

    iki soru yapıldı

---
+++ src/model/Developer.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Developer {
+    private int id;
+    private String name;
+    private double salary;
+
+    public Developer(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void work(){
+        System.out.println("Employee starts to working");
+    }
+}
